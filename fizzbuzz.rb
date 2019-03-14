e47488e137067c75c2f1eb58babc335b8d01f2ba
Rob O'Gara <rgo594@gmail.com>
2019-03-14 15:54:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,11 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 
-def fizzbuzz
- 
+def fizzbuzz()
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif int % 10 == 0
+    return "fizzbuzz"
 end
