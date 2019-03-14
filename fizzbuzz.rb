5b299d9f520dc95c1774d352dbc65fc33b236334
Rob O'Gara <rgo594@gmail.com>
2019-03-14 15:58:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,12 +5,12 @@ require_relative '../fizzbuzz.rb'
 
 def fizzbuzz(int)
   if int % 3 == 0
-    return Fizz
+    return "Fizz"
   elsif int % 5 == 0
-    return Buzz
+    return "Buzz"
   elsif int % 3 == 0 && int % 5 == 0
     return FizzBuzz
   else int % 3 != 0 || int % 5 != 0
-    return nil
+    return "nil"
   end
 end
